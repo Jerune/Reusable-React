8d6e049cbe3fb57af26015c88fd590ba8fd15003
Jeroen van de Sanden <vandesanden@hotmail.com>
2022-11-08 13:58:00 +0100

    Mood Tracker

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import Badge from './components/Badge'
+import Mood from './components/Mood'
 
 function App () {
+  const defaultMoods = ["Great", "Okay", "Bad"]
+
   return (
     <section>
-      <h1>Check out these badges!</h1>
+      <h2>Check out these badges!</h2>
       <Badge color="green">Success</Badge> This is operational. <br />
       <Badge color="red">Removed</Badge> This is critical. <br />
       <Badge color="yellow">Warning</Badge> This is a warning. <br />
       <Badge color="blue">Beta</Badge> This is in progress. <br />
+      
+      <h2>Mood Tracker</h2>
+      <Mood text={"How was your day?"} defaults={defaultMoods}/>
+
+
     </section>
   )
 }
---
+++ src/components/Mood.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react"
+import PropTypes from "prop-types"
+
+export default function Mood ({ text, defaults }) {
+  const [inputValue, setInputValue] = useState('')
+    const maxLength = 280
+  
+    function handleChange(event){
+        setInputValue(event.target.value)
+    }
+
+function pushInput(value){
+    setInputValue(value)
+}
+
+function countCharacters(){
+    return inputValue.split(" ").join("").length
+}
+
+  return <div className="counterInput">
+    <div>
+      {defaults.map((value, index) => {
+        return <button onClick={() => pushInput(value)} key={index}>{value}</button>
+      })}
+    </div>
+    <textarea
+      placeholder={text}
+      value={inputValue}
+      onChange={handleChange}
+    />
+    <div>{countCharacters()}/{maxLength}</div>
+  </div>
+}
+
+Mood.propTypes = {
+    text: PropTypes.string.isRequired,
+    defaults: PropTypes.array
+}
